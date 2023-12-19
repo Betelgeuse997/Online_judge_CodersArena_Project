e54f5dee7e9527cbe9cf889650eb6de998db0d45
Betelgeuse997 <43148054+Betelgeuse997@users.noreply.github.com>
2023-12-19 12:03:53 +0530

    Update BACK_SERVER_URL5

--- Frontend/src/components/Questions/ListQuestion.js
+++ Frontend/src/components/Questions/ListQuestion.js
@@ -27,7 +27,7 @@ const ListQuestion = () => {
     useEffect(() => {
         setLoading(true);
         axios
-            .get(`http://${BACK_SERVER_URL}:5000/question/${id}`, {
+            .get(`https://${BACK_SERVER_URL}:5000/question/${id}`, {
                 headers: {
                   Authorization: `Bearer ${token}`,
                 },
